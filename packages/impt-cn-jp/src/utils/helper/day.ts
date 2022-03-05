32abef15e5511a6a73a43b48f603a7220069cdda
vergil <yan53074.work@gmail.com>
2022-03-05 15:24:29 +0900

    <fix>timeformat

--- packages/impt-cn-jp/src/utils/helper/day.ts
+++ packages/impt-cn-jp/src/utils/helper/day.ts
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs';
 
 export const dayFormat = (date: string, format?: any) => {
-  return dayjs(date).format(format || 'YYYY.MM.DD HH:mm:ss');
+  return date && dayjs(date).format(format || 'YYYY.MM.DD HH:mm:ss');
 };
