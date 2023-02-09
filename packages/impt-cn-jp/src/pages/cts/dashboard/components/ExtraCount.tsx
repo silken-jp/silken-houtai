3f5c51b3bfb4b3b1442f5bcae7b033919c8eaecf
intvergil <yan53074@gmail.com>
2023-02-09 21:37:25 +0900

    <fix>

--- packages/impt-cn-jp/src/pages/cts/dashboard/components/ExtraCount.tsx
+++ packages/impt-cn-jp/src/pages/cts/dashboard/components/ExtraCount.tsx
@@ -1,7 +1,6 @@
-import { Card, Button, Descriptions, Divider } from 'antd';
+import { Card, Button, Descriptions, Divider, Spin } from 'antd';
 import { useRequest } from 'ahooks';
 ////
-import { getDstByDate } from '@/services/request/waybill';
 import { genMabs, getMonthStat } from '@/services/request/mabs';
 
 export interface ExtraCountProps {
@@ -12,14 +11,19 @@ export interface ExtraCountProps {
 }
 
 const ExtraCount: React.FC<ExtraCountProps> = (props) => {
-  const countInfoAPI = useRequest(async () => {
-    const mabs = await getMonthStat({
-      agentId: props?.agentId,
-      flightStartDate: props.startDate,
-      flightEndDate: props?.endDate,
-    });
-    return mabs?.[0] || {};
-  });
+  const countInfoAPI = useRequest(
+    async () => {
+      const mabs = await getMonthStat({
+        agentId: props?.agentId,
+        flightStartDate: props.startDate,
+        flightEndDate: props?.endDate,
+      });
+      return mabs?.[0] || {};
+    },
+    {
+      refreshDeps: [props.agentId],
+    },
+  );
   const genMabsAPI = useRequest(genMabs, {
     manual: true,
   });
@@ -34,49 +38,51 @@ const ExtraCount: React.FC<ExtraCountProps> = (props) => {
   };
 
   return (
-    <Card size="small" loading={countInfoAPI?.loading}>
-      <Descriptions
-        size="small"
-        title={props.title}
-        column={2}
-        extra={
-          <Button
-            loading={genMabsAPI.loading}
-            type="primary"
-            size="small"
-            onClick={handleUpdate}
-          >
-            更新
-          </Button>
-        }
-      >
-        <Descriptions.Item label="件数">
-          {countInfoAPI?.data?.waybillTotalCount || 0}
-        </Descriptions.Item>
-        <Descriptions.Item label="重量">
-          {countInfoAPI?.data?.GWTotalCount || 0}
-        </Descriptions.Item>
-        <Descriptions.Item label="個数">
-          {countInfoAPI?.data?.NOTotalCount || 0}
-        </Descriptions.Item>
-        <Descriptions.Item label="MAWB">
-          {countInfoAPI?.data?.mawbTotalCount || 0}
-        </Descriptions.Item>
-        {/* <Descriptions.Item label="未許可件数">0</Descriptions.Item> */}
-      </Descriptions>
-      <Divider style={{ margin: '12px 0' }} />
-      <Descriptions size="small" column={3}>
-        <Descriptions.Item label="成田">
-          {countInfoAPI?.data?.NRTTotalCount || 0}
-        </Descriptions.Item>
-        <Descriptions.Item label="関西">
-          {countInfoAPI?.data?.KIXTotalCount || 0}
-        </Descriptions.Item>
-        <Descriptions.Item label="羽田">
-          {countInfoAPI?.data?.HNDTotalCount || 0}
-        </Descriptions.Item>
-      </Descriptions>
-    </Card>
+    <Spin spinning={countInfoAPI.loading}>
+      <Card size="small">
+        <Descriptions
+          size="small"
+          title={props.title}
+          column={2}
+          extra={
+            <Button
+              loading={genMabsAPI.loading}
+              type="primary"
+              size="small"
+              onClick={handleUpdate}
+            >
+              更新
+            </Button>
+          }
+        >
+          <Descriptions.Item label="件数">
+            {countInfoAPI?.data?.waybillTotalCount || 0}
+          </Descriptions.Item>
+          <Descriptions.Item label="重量">
+            {countInfoAPI?.data?.GWTotalCount || 0}
+          </Descriptions.Item>
+          <Descriptions.Item label="個数">
+            {countInfoAPI?.data?.NOTotalCount || 0}
+          </Descriptions.Item>
+          <Descriptions.Item label="MAWB">
+            {countInfoAPI?.data?.mawbTotalCount || 0}
+          </Descriptions.Item>
+          {/* <Descriptions.Item label="未許可件数">0</Descriptions.Item> */}
+        </Descriptions>
+        <Divider style={{ margin: '12px 0' }} />
+        <Descriptions size="small" column={3}>
+          <Descriptions.Item label="成田">
+            {countInfoAPI?.data?.NRTTotalCount || 0}
+          </Descriptions.Item>
+          <Descriptions.Item label="関西">
+            {countInfoAPI?.data?.KIXTotalCount || 0}
+          </Descriptions.Item>
+          <Descriptions.Item label="羽田">
+            {countInfoAPI?.data?.HNDTotalCount || 0}
+          </Descriptions.Item>
+        </Descriptions>
+      </Card>
+    </Spin>
   );
 };
 
